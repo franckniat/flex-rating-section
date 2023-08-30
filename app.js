c1df836bd24cb1b4ee76d7af3acb5846ec54b020
franckniat <niatmanuel2002@gmail.com>
2023-08-30 15:37:27 +0100

    Edit animation

--- app.js
+++ app.js
@@ -2,14 +2,12 @@ const item = document.querySelector('.item3-1');
   const contenus = item.querySelectorAll('.item3-1-1');
   let currentIndex = 0;
 
-  setInterval(() => {
-    contenus[currentIndex].style.opacity = '0';
-    contenus[currentIndex].style.display = 'none';
-    contenus[currentIndex].style.transtion = 'ease 0.1s';
-
-    setTimeout(() => {
-      currentIndex = (currentIndex + 1) % contenus.length;
-      contenus[currentIndex].style.opacity = '1';
-      contenus[currentIndex].style.display = 'flex';
+    setInterval(() => {
+        contenus[currentIndex].classList.add('active');
+        setTimeout(() => {
+          currentIndex = (currentIndex + 1) % contenus.length;
+          contenus[currentIndex].classList.add('active');
+        }, 1500);
+        contenus[currentIndex].classList.remove('active');
     }, 1500);
-}, 1500);
+  
